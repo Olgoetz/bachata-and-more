d35ea01106dba925db8008747e2e78b318ffd157
Oliver Götz <goetzoliver89@gmail.com>
2022-11-10 09:12:50 +0100

    viewport meta tag added

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
 import '../styles/index.css';
 
 function MyApp({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+	return (
+		<>
+			<Head>
+				<meta
+					name='viewport'
+					content='width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0'
+				/>
+			</Head>
+			<Component {...pageProps} />;
+		</>
+	);
 }
 
 export default MyApp;
