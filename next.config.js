30b5e4f8ad5e7e73eaae67eb24e947f39c195d5e
Oliver Götz <goetzoliver89@gmail.com>
2022-11-30 00:48:36 +0100

    github actions added

--- next.config.js
+++ next.config.js
@@ -1,14 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+module.exports = {
 	reactStrictMode: true,
 	images: {
 		unoptimized: true,
 	},
-};
-
-const withVideos = require('next-videos');
-
-module.exports = {
-	nextConfig,
-	withVideos: withVideos(),
 };
