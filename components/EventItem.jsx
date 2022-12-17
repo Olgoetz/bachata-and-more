3beb11dea42b07bc7e6ae504dc39921958ba2125
Oliver Götz <goetzoliver89@gmail.com>
2022-12-17 17:24:52 +0100

    events added

--- components/EventItem.jsx
+++ components/EventItem.jsx
@@ -7,10 +7,12 @@ const EventItem = ({ image, title, date, address, bookingLink }) => {
 	const formatedAddress = address.replace(', ', '\n');
 
 	return (
-		<li className='flex shadow-xl round-lg p-3 my-3'>
-			<Image src={image} width={250} height={100} />
-			<div className='flex flex-col ml-4 w-full gap-3'>
-				<h2 className='text-2xl font-bold mb-3'>{title}</h2>
+		<li className='flex flex-col items-center md:flex-row shadow-xl round-lg p-3 my-3'>
+			<Image src={image} width={250} height={150} />
+			<div className='flex flex-col md:ml-4 w-full gap-3'>
+				<h2 className='text-lg text-center md:text-left md:text-2xl font-bold'>
+					{title}
+				</h2>
 				<div className='flex items-center'>
 					<MdDateRange size={20} className='mr-1' />
 					<time className='font-bold'>{date}</time>
@@ -19,7 +21,7 @@ const EventItem = ({ image, title, date, address, bookingLink }) => {
 					<GrLocation size={20} className='mr-1' />
 					<address className='text-sm'>{formatedAddress}</address>
 				</div>
-				<button className='bg-orange-400 mt-3 w-[50%] text-black text-sm md:text-lg font-bold rounded-md px-4 py-2 uppercase hover:bg-orange-300 active:bg-orange-500'>
+				<button className='bg-orange-400 mx-auto justify-content w-[50%] text-black text-sm md:text-lg font-bold rounded-md px-3 py-1 uppercase hover:bg-orange-300 active:bg-orange-500'>
 					<a href={bookingLink} target='_blank' rel='noreferrer'>
 						Buchen
 					</a>
