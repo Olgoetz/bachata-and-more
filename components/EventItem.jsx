e91042cc3ff0e275bd8681d700b6764c17cce4df
Oliver Götz <goetzoliver89@gmail.com>
2022-12-20 18:59:38 +0100

    data added

--- components/EventItem.jsx
+++ components/EventItem.jsx
@@ -11,7 +11,7 @@ const EventItem = ({ image, title, venue, date, address, bookingLink }) => {
 			<Image src={image} width={400} height={1} />
 			<div className='flex flex-col md:ml-4 w-full gap-2'>
 				<h2 className='text-lg mt-1 md:text-2xl font-bold'>{venue}</h2>
-				<h3 className='text-lg italic'>"{title}"</h3>
+				<h3 className='text-lg italic'>&quot;{title}&quot;</h3>
 				<div className='flex items-center justify-start md:justify-start gap-0.5'>
 					<MdDateRange size={20} className='mr-1' />
 					<time className='font-bold'>{date}</time>
