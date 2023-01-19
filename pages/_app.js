724f17e5da187d4253ff4650aded8fe2b63f91f0
Oliver Götz <goetzoliver89@gmail.com>
2023-01-19 21:44:27 +0100

    meta data adjusted

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import Head from 'next/head';
 import Navbar from '../components/layout/Navbar';
 import Footer from '../components/layout/Footer';
 import '../styles/globals.css';
-
+import Hero from '../public/images_original/MichiyOli_hero.jpg';
 function MyApp({ Component, pageProps }) {
 	return (
 		<>
@@ -13,17 +13,14 @@ function MyApp({ Component, pageProps }) {
 					content='Workout-, Yoga- sowie Bachatakurse für Events'
 				/>
 				<meta name='viewport' content='width=device-width, initial-scale=1.0' />
-				<meta property='og:title' content='Bachata & More' />
+				{/* <meta property='og:title' content='Bachata & More' />
 				<meta property='og:url' content='https://www.bachata-and-more.de' />
 				<meta
 					property='og:description'
 					content='Workout-, Yoga- sowie Bachatakurse für Events'
-				/>
+				/> */}
 				<meta property='og:type' content='image/jpg' />
-				<meta
-					property='og:image'
-					content='../public/images_original/MichiyOli_hero.jpg'
-				/>
+				<meta property='og:image' content={Hero} />
 				<meta property='og:locale' content='de_DE' />
 			</Head>
 			<Navbar />
