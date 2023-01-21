81ee7588cd3d7579c00aefb7998e69dab0ee7beb
Oliver Götz <goetzoliver89@gmail.com>
2023-01-21 10:44:14 +0100

    accordion added

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ module.exports = {
 	},
 	env: {
 		EMAIL_SOURCE_ADDRESS: 'no_reply@bachata-and-more.de',
-		EMAIL_RECIPIENT_ADDRESSES: ['oli@bachata-and-more.de'],
+		EMAIL_RECIPIENT_ADDRESSES: ['info@bachata-and-more.de'],
 		EMAIL_API_ENDPOINT:
 			'https://19l17oesb8.execute-api.eu-central-1.amazonaws.com/prod/email',
 	},
