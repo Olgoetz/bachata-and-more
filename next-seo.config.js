28ef39699fc88c1016eaaf52d2cf8ebf440ad19b
Oliver Götz <olivergoetz@OliversAir2022.fritz.box>
2023-02-09 13:31:49 +0100

    seo added

---
+++ next-seo.config.js
@@ -0,0 +1,23 @@
+export default {
+    title: 'Bachata & More',
+    description: 'Eventkonzept für Workout-, Yoga- und Bachatakurse',
+    openGraph: {
+      type: 'website',
+      locale: 'de_IE',
+      url: 'https://www.bachata-and-more.de',
+      siteName: 'Bachata & More',
+      images: [
+        {
+          url: 'https://bachata-and-more.s3.eu-central-1.amazonaws.com/pictures/MichiyOli_hero.png',
+          width: 800,
+          height: 600,
+          alt: 'Bachata & More',
+        },
+      ],
+    },
+    twitter: {
+        handle: '@handle',
+        site: '@site',
+        cardType: 'summary_large_image',
+    }
+  };
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,11 @@
-import Head from 'next/head';
+
 import Navbar from '../components/layout/Navbar';
 import Footer from '../components/layout/Footer';
 import '../styles/globals.css';
 
 import { Raleway } from '@next/font/google';
-
+import {DefaultSeo} from 'next-seo';
+import SEO from '../next-seo.config';
 const raleway = Raleway({
 	subsets: ['latin'],
 	display: 'swap',
@@ -14,29 +15,8 @@ const raleway = Raleway({
 function MyApp({ Component, pageProps }) {
 	return (
 		<>
-			<Head>
-				<title>Bachata and More</title>
-				<meta
-					name='description'
-					content='Eventkonzept für Workout-, Yoga- und Bachatakurse'
-				/>
-				<meta name='viewport' content='width=device-width, initial-scale=1.0' />
-				<meta property='og:title' content='Bachata & More' />
-				<meta property='og:url' content='https://www.bachata-and-more.de' />
-				<meta
-					property='og:description'
-					content='Workout-, Yoga- sowie Bachatakurse für Events'
-				/>
-				<meta property='og:type' content='image/png' />
-				<meta
-					property='og:image'
-					content='https://bachata-and-more.s3.eu-central-1.amazonaws.com/pictures/MichiyOli_hero.png'
-				/>
-				<meta property='og:image:width' content='600' />
-
-				<meta property='og:image:height' content='300' />
-				<meta property='og:locale' content='de_DE' />
-			</Head>
+			
+			<DefaultSeo {...SEO} />
 			<main className={`${raleway.variable} font-sans`}>
 				<Navbar />
 				<Component {...pageProps} />
