e68b5f38f0949da428b14cb360eade320a17a08e
Oliver Götz <olivergoetz@OliversAir2022.fritz.box>
2023-03-09 22:45:49 +0100

    marketing kit added

--- components/ui/DownloadConceptButton.jsx
+++ components/ui/DownloadConceptButton.jsx
@@ -3,11 +3,11 @@ const DownloadConceptButton = (props) => {
 		<>
 			<button className='bg-orange-400 text-black text-sm md:text-lg font-bold rounded-md px-6 py-3 uppercase hover:bg-orange-300 active:bg-orange-500'>
 				<a
-					href='https://bachata-and-more.s3.eu-central-1.amazonaws.com/documents/Bachata%26More.pdf'
+					href='https://bachata-and-more.s3.eu-central-1.amazonaws.com/marketing-kit/bachata-marketing-kit.zip'
 					target='_blank'
 					rel='noreferrer'
 				>
-					Download Konzept
+					Download Marketing Kit
 				</a>
 			</button>
 		</>
